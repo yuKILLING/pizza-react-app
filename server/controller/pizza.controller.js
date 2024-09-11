db03179f09a1700f1585d3da1e3b723ba034c6dd
Rudee <eng1ne11@mail.ru>
2024-09-11 23:18:32 +0400

    fixed bugs, and added some features

--- server/controller/pizza.controller.js
+++ server/controller/pizza.controller.js
@@ -5,7 +5,7 @@ class PizzaController {
       const pizzasList = await db.query("SELECT * FROM pizzas");
       res.status(201).json(pizzasList.rows);
     } catch (error) {
-      console.error(error)
+      console.error(error);
       res.status(500).json({ error: error.message });
     }
   }
@@ -16,9 +16,9 @@ class PizzaController {
         pizza_name,
         pizza_description,
         pizza_price = 0,
-        pizza_status = 'none',
+        pizza_status = "none",
         pizza_img,
-        pizza_grams
+        pizza_grams,
       } = req.body;
       const query =
         "INSERT INTO pizzas (pizza_name, pizza_description, pizza_price, pizza_status, pizza_img, pizza_grams) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *";
@@ -28,7 +28,7 @@ class PizzaController {
         pizza_price,
         pizza_status,
         pizza_img,
-        pizza_grams
+        pizza_grams,
       ];
       const newPizza = await db.query(query, values);
       res.status(201).json(newPizza.rows[0]);
@@ -37,5 +37,25 @@ class PizzaController {
       res.status(500).json({ error: error.message });
     }
   }
+
+  async updatePizzaOrders(req, res) {
+    try {
+      const cart = req.body;
+      console.log(cart)
+      for (const pizza of cart) {
+        const { pizza_name } = pizza;
+        const query =
+          "UPDATE pizzas SET pizza_orders = pizza_orders + 1 WHERE pizza_name = $1 RETURNING *";
+        const values = [pizza_name];
+        await db.query(query, values);
+      }
+
+      res.status(200).json({ message: "Pizza orders updated successfully" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: error.message });
+    }
+  }
 }
+
 module.exports = new PizzaController();
--- server/routes/pizzaRouter.js
+++ server/routes/pizzaRouter.js
@@ -6,6 +6,7 @@ const PizzaController = require("../controller/pizza.controller");
 // Pizza's table routes
 router.get("/getallpizzas", PizzaController.getAllPizzas);
 router.post("/addpizza", PizzaController.addPizza);
+router.post("/addorder", PizzaController.updatePizzaOrders);
 
 
 module.exports = router;
